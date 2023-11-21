d73890733a7cb24ca2c9a17b20529527dbef181c
skr1pt11 <skr1pt11@list.ru>
2023-11-22 02:14:34 +0800

    Declaration component

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,3 +1,71 @@
 <template>
-  
+  <div id="newtask">
+    <input type="text" placeholder="Task to be done.." v-model.trim="task.title" />
+    <button id="push" @click="addTask">Add Task</button>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      task: {
+        title: "",
+      },
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.task.title.length === 0) return;
+      this.task.id = new Date().getTime();
+      this.task.time = new Date().getHours() + ":" + new Date().getMinutes();
+      this.$emit("create", this.task);
+      this.task = {
+        title: "",
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+#newtask {
+  position: relative;
+  background-color: #ffffff;
+  padding: 30px 20px;
+  border-radius: 5px;
+  box-shadow: 0 15px 30px rgba(0, 0, 0, 0.3);
+}
+#newtask input {
+  width: 70%;
+  width: 70%;
+  height: 45px;
+  font-family: "Poppins", sans-serif;
+  font-size: 15px;
+  border: 2px solid #d1d3d4;
+  padding: 12px;
+  color: #111111;
+  font-weight: 500;
+  position: relative;
+  border-radius: 5px;
+}
+#newtask input:focus {
+  outline: none;
+  border-color: #8052ec;
+}
+
+#newtask button {
+  position: relative;
+  float: right;
+  width: 20%;
+  height: 45px;
+  border-radius: 5px;
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  background-color: #8052ec;
+  border: none;
+  color: #ffffff;
+  cursor: pointer;
+  outline: none;
+}
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="tasks" v-for="task in tasks" :key="task.id">
+    <p><strong>Название задачи: </strong> {{ task.title }}</p>
+    <p><strong>Дата публикации: </strong>{{ task.time }}</p>
+    <!-- <button class="delete"><img src="../img/trash.jpg" alt="trash" /></button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+#tasks{
+    background-color: #ffffff;
+    padding: 30px 20px;
+    margin-top: 60px;
+    border-radius: 10px;
+    box-shadow: 0 15px 30px rgba(0,0,0,0.3);
+    width: 100%;
+    position: relative;
+}
+</style>
